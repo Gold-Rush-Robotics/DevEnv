8d80073c803719bf44fbcc154db0710754c7cf07
nchan18 <nchan18@outlook.com>
2024-09-17 22:41:48 -0400

    version bump

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Pull the actual image so it is saved locally
-docker pull ghcr.io/roboeagles4828/developer-environment:7
+docker pull ghcr.io/roboeagles4828/developer-environment:8
 mkdir -p ~/docker/vscode
 echo "Display to use: $DISPLAY"
