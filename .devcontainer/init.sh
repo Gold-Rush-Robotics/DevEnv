3cf88e39678d5df1c5a5fdfb68c215bb3a0cdfcb
ZacHowardUNCC <zhoward4@charlotte.edu>
2025-11-04 20:28:33 -0500

    image changed

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Pull the actual image so it is saved locally
-docker pull ghcr.io/gold-rush-robotics/dev_env:2
+docker pull ghcr.io/gold-rush-robotics/dev_env:5
 mkdir -p ~/docker/vscode
 echo "Display to use: $DISPLAY"
